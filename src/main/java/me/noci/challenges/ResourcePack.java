3746a34c7b7c22af5a273e462cb0953054e3b111
Noci <nocigit@gmail.com>
2024-05-07 16:19:09 +0200

    feat: Updated resource pack

--- src/main/java/me/noci/challenges/ResourcePack.java
+++ src/main/java/me/noci/challenges/ResourcePack.java
@@ -9,7 +9,7 @@ import net.kyori.adventure.text.format.TextColor;
 public final class ResourcePack {
 
     public static final String URL = "https://github.com/xNoci/QuickChallenge/raw/main/ResourcePack.zip";
-    public static final String HASH = "772F2C6F31AF6E242232989C5CE3C7E3AC6134D4";
+    public static final String HASH = "9B9BFA28148E866949661572F2FC0C4EACA22685";
     public static final boolean REQUIRED = true;
     public static final Component PROMPT = Component.text()
             .append(Component.text("QuickChallenge Resource Pack", TextColor.color(195, 49, 214)))
