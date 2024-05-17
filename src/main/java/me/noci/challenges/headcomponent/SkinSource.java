52d31e11b9ea632ebfa09f434177ac1833f97258
Noci <nocigit@gmail.com>
2024-05-18 00:06:20 +0200

    chore: removed debug code

--- src/main/java/me/noci/challenges/headcomponent/SkinSource.java
+++ src/main/java/me/noci/challenges/headcomponent/SkinSource.java
@@ -5,7 +5,6 @@ import lombok.SneakyThrows;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.UUID;
 
 public enum SkinSource {
@@ -39,12 +38,6 @@ public enum SkinSource {
     @FunctionalInterface
     private interface UrlMapper {
         String map(String uuid, boolean overlay);
-    }
-
-    public static void main(String[] args) {
-        Arrays.stream(SkinSource.MINOTAR.colors(UUID.fromString("7096e2c7-bbe3-431b-83e2-2bd7422abcd4"), true))
-                .mapToObj(Integer::toHexString)
-                .forEach(System.out::println);
     }
 
 }
