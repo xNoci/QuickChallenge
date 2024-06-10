c6db622dd858d385714eb6e7119d90bea0d54506
Noci <nocigit@gmail.com>
2024-06-10 16:06:15 +0200

    build: cleaned up code

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,13 +34,13 @@ tasks {
         archiveFileName.set("${project.property("plugin.name")}-${project.version}.jar")
 
         if (project.hasProperty("pluginFolder")) {
-            val pluginFolder = project.property("pluginFolder") as String?;
+            val pluginFolder = project.property("pluginFolder") as String?
             if (!pluginFolder.isNullOrBlank()) {
                 destinationDirectory.set(file(pluginFolder))
             }
         }
 
-        fun reloc(pkg: String) = relocate(pkg, "${project.group}.dependency.$pkg");
+        fun reloc(pkg: String) = relocate(pkg, "${project.group}.dependency.$pkg")
         reloc("com.cryptomorin.xseries")
         reloc("io.papermc.lib")
 
