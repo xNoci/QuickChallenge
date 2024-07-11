2233940dc10ccb884255eb9307e82e1140ad9038
Noci <nocigit@gmail.com>
2024-07-11 11:54:31 +0200

    refactor: Updated XSeries to 11.2.0

--- src/main/java/me/noci/challenges/gui/InventoryConstants.java
+++ src/main/java/me/noci/challenges/gui/InventoryConstants.java
@@ -1,15 +1,13 @@
 package me.noci.challenges.gui;
 
-import com.cryptomorin.xseries.SkullUtils;
+import com.cryptomorin.xseries.profiles.builder.XSkull;
+import com.cryptomorin.xseries.profiles.objects.Profileable;
 import me.noci.challenges.settings.Option;
 import me.noci.quickutilities.inventory.GuiItem;
 import me.noci.quickutilities.inventory.GuiProvider;
 import me.noci.quickutilities.utils.QuickItemStack;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.Objects;
 
 public class InventoryConstants {
 
@@ -22,9 +20,7 @@ public class InventoryConstants {
 
     public static QuickItemStack woodPauseSkull() {
         QuickItemStack item = new QuickItemStack(Material.PLAYER_HEAD);
-        Objects.requireNonNull(item, "Item cannot be null");
-        ItemMeta itemMeta = SkullUtils.applySkin(item.getItemMeta(), "c77aae1a26b952493a7371c30ad8c491f12b574cc94a41b2f91a373ca68f9098");
-        item.setItemMeta(itemMeta);
+        XSkull.of(item).profile(Profileable.detect("c77aae1a26b952493a7371c30ad8c491f12b574cc94a41b2f91a373ca68f9098")).apply();
         return item;
     }
 
